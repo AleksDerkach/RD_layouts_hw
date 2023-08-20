508cda8eee299d79cc642d0880d3d75031792106
alexderkach <derkachalex@i.ua>
2023-08-20 12:34:07 +0300

    Home work

---
+++ app/src/main/java/com/example/task_11_hometask_layots/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.task_11_hometask_layots
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activitygt)
+    }
+
+
+}
